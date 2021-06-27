f1898964df6e28971b22d99109177fd37deb2662
vignesh-2205 <83761210+vignesh-2205@users.noreply.github.com>
2021-06-27 18:55:48 +0300

    Add files via upload

--- delta-2_script.js
+++ delta-2_script.js
@@ -1,42 +1,124 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+let highscore_disp = document.getElementById("Highscore");
+let score_disp = document.getElementById("Score");
+var score = 0;
+let start;
 var gravity = 1;
 var playerY = 425;
+var ceiling_hole_size;
+var floor_hole_size;
 const playerX = 50;
-let rect_fh_x = 850;
+let rect_fh_x = 1000;
 let rect_fh_y = 500;
-let rect_ch_x = 850
+let rect_ch_x = 1600;
 let rect_ch_y = 0;
+
+var chole_spawntime = 750;
+var fhole_spawntime = 750;
+ceiling_hole_size = getRandomint(200, 501);
+floor_hole_size = getRandomint(200, 501);
+
 
 
 function draw() {
     ctx.clearRect(0, 0, 1000, 600);
+    score_count();
     gravity_effect();
     player_Generate();
     draw_floor_ceiling();
+    hole_initialize();
     draw_hole_ceiling();
     update();
     draw_hole_floor();
+    lose_Condition();
+
 
 
 
 }
-setInterval(draw, 10);
-setInterval(holes, 11500);
+var Interval = setInterval(draw, 10);
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function startGm() {
+    window.addEventListener('keyup', function() {
+        start = 1;
+    })
+};
+
+
+function getRandomint(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
-function holes() {
+function score_count() {
+    highscore = localStorage.getItem("hscore");
+    if (score > highscore) {
+        highscore = score;
+    }
+    highscore_disp.innerText = highscore;
+    score_disp.innerText = score;
+    score++;
+
+}
+
+function lose_Condition() {
+    if (playerX + 60 >= rect_fh_x && playerX <= rect_fh_x + floor_hole_size && playerY == 425) {
+        alert("GAME OVER!! SCORE: " + score);
+        if (score > highscore) {
+            highscore = score;
+            highscore_disp.innerText = highscore;
+            localStorage.setItem("hscore", highscore);
+        }
+        document.location.reload();
+        clearInterval(Interval);
+    }
+    if (playerX + 60 >= rect_ch_x && playerX <= rect_ch_x + ceiling_hole_size && playerY == 100) {
+        alert("GAME OVER!! SCORE: " + score);
+        if (score > highscore) {
+            highscore = score;
+            highscore_disp.innerText = highscore;
+            localStorage.setItem("hscore", highscore);
+        }
+        document.location.reload();
+        clearInterval(Interval);
+    }
+}
+
+
+function hole_initialize() {
+    fhole_spawntime--;
+    if (fhole_spawntime <= 0) {
+        rect_fh_x = 1000;
+        floor_hole_size = getRandomint(200, 501);
+        fhole_spawntime = 750;
+    }
+    if (fhole_spawntime <= 375 || chole_spawntime < 750) {
+        chole_spawntime--;
+
+
+    }
+    if (chole_spawntime <= 0) {
+        rect_ch_x = 1000;
+        ceiling_hole_size = getRandomint(200, 501);
+        chole_spawntime = 750;
+
+    }
+}
+
+function holes_floor() {
     rect_fh_x = 1000;
-
-
+    floor_hole_size = getRandomint(200, 501);
 }
+
+function holes_ceiling() {
+    rect_ch_x = 1000;
+    ceiling_hole_size = getRandomint(200, 501);
+}
+
 
 function update() {
-    rect_fh_x -= 1;
-    rect_ch_x -= 1;
+    rect_fh_x -= 2;
+    rect_ch_x -= 2;
 
 }
 
@@ -51,12 +133,12 @@ function draw_floor_ceiling() {
 function draw_hole_floor() {
     ctx.fillStyle = '#eee';
 
-    ctx.fillRect(rect_fh_x, rect_fh_y, 150, 100);
+    ctx.fillRect(rect_fh_x, rect_fh_y, floor_hole_size, 100);
 }
 
 function draw_hole_ceiling() {
     ctx.fillStyle = '#eee';
-    ctx.fillRect(rect_ch_x, rect_ch_y, 150, 100);
+    ctx.fillRect(rect_ch_x, rect_ch_y, ceiling_hole_size, 100);
 }
 
 function player_Generate() {
@@ -78,13 +160,13 @@ document.addEventListener("keydown", gravity_shift, false);
 
 function gravity_shift(e) {
     if (e.code == "Space") {
-        console.log(1);
+
         if (playerY == 425) {
-            console.log(3);
+
             gravity = 0;
         }
         if (playerY == 100) {
-            console.log(2);
+
             gravity = 1;
         }
 
